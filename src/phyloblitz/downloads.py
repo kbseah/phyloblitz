b7923d1b120e30004ebfd2310ae8ba3029cff997
Brandon Seah <brandon_seah@tll.org.sg>
2026-02-24 09:36:25 +0800

    Format indentation

--- src/phyloblitz/downloads.py
+++ src/phyloblitz/downloads.py
@@ -3,6 +3,7 @@ import requests
 
 logger = logging.getLogger(__name__)
 
+
 def list_versions():
     """List available versions of reference databases on Zenodo.
 
@@ -14,30 +15,31 @@ def list_versions():
     if response.status_code == 200:
         data = response.json()
         versions = {}
-        for hit in data['hits']['hits']:
-            versions[hit['metadata']['version']] = {
-                'version': hit['metadata']['version'],
-                'doi': hit['metadata']['doi'],
-                'created': hit['created'],
-                'files': []
+        for hit in data["hits"]["hits"]:
+            versions[hit["metadata"]["version"]] = {
+                "version": hit["metadata"]["version"],
+                "doi": hit["metadata"]["doi"],
+                "created": hit["created"],
+                "files": [],
             }
-            for file in hit['files']:
-                if 'SSU' in file['key']:
-                    marker='SSU'
-                elif 'LSU' in file['key']:
-                    marker='LSU'
+            for file in hit["files"]:
+                if "SSU" in file["key"]:
+                    marker = "SSU"
+                elif "LSU" in file["key"]:
+                    marker = "LSU"
                 else:
                     logger.warning(f"Unknown marker type in file: {file['key']}")
-                    marker='Unknown'
-                versions[hit['metadata']['version']]['files'].append({
-                    'marker': marker,
-                    'filename': file['key'],
-                    'size': file['size'],
-                    'checksum': file['checksum'],
-                    'download_url': file['links']['self'],
-                })
+                    marker = "Unknown"
+                versions[hit["metadata"]["version"]]["files"].append(
+                    {
+                        "marker": marker,
+                        "filename": file["key"],
+                        "size": file["size"],
+                        "checksum": file["checksum"],
+                        "download_url": file["links"]["self"],
+                    }
+                )
         return versions
     else:
         logger.error(f"Failed to fetch versions: {response.status_code}")
         return []
-
